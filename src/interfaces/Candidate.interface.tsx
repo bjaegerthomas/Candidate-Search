eb220dcd41b50d849d2f9ffbcfae772c68938de9
jaegerthomas <bjaegerthomas@gmail.com>
2025-02-04 10:22:26 -0500

    adding code for Candiidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly name: string | null;
+    readonly username: string | null;
+    readonly location: string | null;
+    readonly avatar: string | null;
+    readonly email: string | null;
+    readonly html_url: string | null;
+    readonly company: string | null;
+    }
