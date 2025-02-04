c4e9acf11fc3c6e9563c293211e468ef520d007a
jaegerthomas <bjaegerthomas@gmail.com>
2025-02-04 10:23:27 -0500

    initial code for nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,39 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
+  
   return (
-    <div>Nav</div>
-  )
+    <nav>
+    <ul className='nav nav-tabs'>
+      <li className='nav-item'>
+        <h2>
+          <Link
+            to='/'
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            HOME
+          </Link>
+        </h2>
+      </li>
+      <li className='nav-item'>
+        <h2>
+          <Link
+            to='/WatchList'
+            className={
+              currentPage === '/WatchList' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Potential Candidates
+          </Link>
+        </h2>
+      </li>
+    </ul>
+  </nav>
+);
 };
 
+
 export default Nav;
+
