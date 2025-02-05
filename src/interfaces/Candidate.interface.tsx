e63ada86e9b822dd2ec2f95226bfe34ebbe60802
jaegerthomas <bjaegerthomas@gmail.com>
2025-02-05 14:58:46 -0500

    added blank component files, and updating candidatesearch.tsx

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -3,7 +3,6 @@ export default interface Candidate {
     readonly username: string | null;
     readonly location: string | null;
     readonly avatar: string | null;
-    readonly email: string | null;
     readonly html_url: string | null;
     readonly company: string | null;
     }
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,41 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import CandidateCard from '../components/CandidateCard';
+import Candidate from '../types/Candidate';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+    name: '',
+    username: '',
+    location: '',
+    avatar: '',
+    html_url: '',
+    company: '',
+  });
+
+  const candidateList =
+
+
+  const addToSavedList = () => {
+    let parsedSavedCandidates: Candidate[] = [];
+    const storedSavedCandidates = localStorage.getItem('savedCandidates');
+    if (typeof storedSavedCandidates === 'string') {
+      parsedSavedCandidates = JSON.parse(storedSavedCandidates);
+    }
+    parsedSavedCandidates.push(currentCandidate);
+    localStorage.setItem('savedCandidates', JSON.stringify(parsedSavedCandidates));
+  };
+
+  return (
+    <>
+      <h1>CandidateSearch</h1>;
+      <CandidateCard
+        currentFilm={currentFilm}
+        addToWatchList={addToWatchList}
+        addToSeenItList={addToSeenItList}
+      />
+    </>
+  );
 };
 
 export default CandidateSearch;
